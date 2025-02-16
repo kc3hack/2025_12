e47376c2597baf1ff921cf7820958fcfa2a4d9b3
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-02-17 02:33:11 +0900

    feat: Userデータ保存ロジックの実装

--- server/chat/src/main.rs
+++ server/chat/src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use db::{DBOption, DB};
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().unwrap();
+
+    let _db = DB::from_option(DBOption {
+        max_connections: 10,
+        acquire_timeout: Duration::from_secs(3),
+    })
+    .await;
+}
---
+++ server/db/src/fixtures/user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, nickname, introduction, created_at) VALUES (
+    '0', 'John Smith Jonson', NULL, DEFAULT
+)
