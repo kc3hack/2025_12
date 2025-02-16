96b7d6a72f3b817ac57e3962db02e393668aa250
きたむらりょうき <>
2025-02-16 18:27:01 +0900

    refactor:_を消した

--- web/src/features/routes/root/message-container.tsx
+++ web/src/features/routes/root/message-container.tsx
@@ -7,18 +7,15 @@ import style from "./messagecontainer.module.scss";
 type Props = { bottomRef: RefObject<HTMLDivElement | null> };
 
 export const MessageContainer = (props: Props) => {
-	const [messages, _] = useAtom(messagesAtom);
-	return (
-		<div className={style.message_container}>
-			{messages.map((message) => (
-				<div
-					key={message.id}
-					className={`${style.message} ${message.is_me && style.is_me}`}
-				>
-					<p>{message.content}</p>
-				</div>
-			))}
-			<div ref={props.bottomRef} />
-		</div>
-	);
+  const [messages] = useAtom(messagesAtom);
+  return (
+    <div className={style.message_container}>
+      {messages.map(message => (
+        <div key={message.id} className={`${style.message} ${message.is_me && style.is_me}`}>
+          <p>{message.content}</p>
+        </div>
+      ))}
+      <div ref={props.bottomRef} />
+    </div>
+  );
 };
