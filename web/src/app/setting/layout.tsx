9264b36aeea96a8c2448ec67121b9e85cfe73b8f
MgmgCandy <hayato.yamaoka.0316@gmail.com>
2025-02-16 19:04:18 +0900

    sidebar-comp

---
+++ web/src/app/setting/layout.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+import style from "../../features/routes/settings-scss/page.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const SettingsSidebar = () => {
+  const Router = useRouter();
+  return (
+    <div className={style.setting_container}>
+      <button type="button" className={style.setting1} onClick={() => Router.push("./profile")}>
+        Profile
+      </button>
+      <button type="button" className={style.setting2} onClick={() => Router.push("./account")}>
+        Account
+      </button>
+      <button type="button" className={style.setting2}>
+        項目3
+      </button>
+      <button type="button" className={style.setting2}>
+        項目4
+      </button>
+      <button type="button" className={style.setting2}>
+        項目5
+      </button>
+      <button type="button" className={style.setting2}>
+        項目2
+      </button>
+      <button type="button" className={style.setting2}>
+        項目2
+      </button>
+      <button type="button" className={style.setting2}>
+        項目2
+      </button>
+      <button type="button" className={style.setting2}>
+        項目2
+      </button>
+    </div>
+  );
+};
+
+const SettingsLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <div className={style.layout}>
+        <div className={style.container}>
+          <SettingsSidebar />
+          <div className={style.contents}>{children}</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default SettingsLayout;
---
+++ web/src/app/setting/profile/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import style from "../../../features/routes/settings-scss/page.module.scss";
+
+const Profile = () => {
+  return (
+    <div className={style.profile_container}>
+      <p className={style.profile}>My Profile</p>
+    </div>
+  );
+};
+
+export default Profile;
