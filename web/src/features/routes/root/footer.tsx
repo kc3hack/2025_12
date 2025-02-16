ac009f11b92d3cbdba11ce572b8efa5734d9f12d
mutyuki <belandinging01@gmail.com>
2025-02-16 20:06:05 +0900

    feat: アイコンを相手のだけ表示させるように作成

--- web/src/features/routes/root/footer.tsx
+++ web/src/features/routes/root/footer.tsx
@@ -13,57 +13,59 @@ import type { Message } from "../../message/type";
 type Props = { bottomRef: RefObject<HTMLDivElement | null> };
 
 export const Footer = (props: Props) => {
-	const [messages, setMessages] = useAtom(messagesAtom);
-	const inputRef = useRef<HTMLTextAreaElement>(null);
-	const [isComposing, setIsComposing] = useState(false);
+  const [messages, setMessages] = useAtom(messagesAtom);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const [isComposing, setIsComposing] = useState(false);
 
-	const handleSubmit = () => {
-		if (!inputRef.current) {
-			return;
-		}
-		if (inputRef.current.value === "") {
-			return;
-		}
-		const newMessage: Message = {
-			id: uuidv4(),
-			author: "me",
-			content: inputRef.current.value,
-			is_me: true,
-		};
-		inputRef.current.value = "";
-		setTimeout(() => {
-			props.bottomRef.current?.scrollIntoView();
-		}, 0);
-		const yesMessage: Message = {
-			id: uuidv4(),
-			author: "bot",
-			content: Math.random() < 0.5 ? "はい" : "いいえ",
-			is_me: false,
-		};
-		setMessages([...messages, newMessage, yesMessage]);
-	};
-	const handleKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
-		if (event.shiftKey) {
-			return;
-		}
-		if (event.key === "Enter" && !isComposing) {
-			event.preventDefault();
-			handleSubmit();
-		}
-	};
-	return (
-		<div className={style.footer}>
-			<Textarea
-				className={style.text_area}
-				placeholder="Type your message here."
-				onKeyDown={handleKeyDown}
-				onCompositionStart={() => setIsComposing(true)}
-				onCompositionEnd={() => setIsComposing(false)}
-				ref={inputRef}
-			/>
-			<Button onClick={handleSubmit} className={style.send_button}>
-				<SendHorizontal />
-			</Button>
-		</div>
-	);
+  const handleSubmit = () => {
+    if (!inputRef.current) {
+      return;
+    }
+    if (inputRef.current.value === "") {
+      return;
+    }
+    const newMessage: Message = {
+      id: uuidv4(),
+      author: "me",
+      content: inputRef.current.value,
+      is_me: true,
+      icon: "https://github.com/shadcn.png"
+    };
+    inputRef.current.value = "";
+    setTimeout(() => {
+      props.bottomRef.current?.scrollIntoView();
+    }, 0);
+    const yesMessage: Message = {
+      id: uuidv4(),
+      author: "bot",
+      content: Math.random() < 0.5 ? "はい" : "いいえ",
+      is_me: false,
+      icon: "https://github.com/shadcn.png"
+    };
+    setMessages([...messages, newMessage, yesMessage]);
+  };
+  const handleKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.shiftKey) {
+      return;
+    }
+    if (event.key === "Enter" && !isComposing) {
+      event.preventDefault();
+      handleSubmit();
+    }
+  };
+  return (
+    <div className={style.footer}>
+      <Textarea
+        className={style.text_area}
+        placeholder="Type your message here."
+        onKeyDown={handleKeyDown}
+        onCompositionStart={() => setIsComposing(true)}
+        onCompositionEnd={() => setIsComposing(false)}
+        ref={inputRef}
+      />
+      <Button onClick={handleSubmit} className={style.send_button}>
+        <SendHorizontal />
+      </Button>
+    </div>
+  );
 };
