03c247a77ec32981d3c4feb76e88cf195c550b3f
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-02-17 15:02:20 +0900

    chore: messagesテーブルにreply_to_idを追加

--- db/migrations/000004_messages.up.sql
+++ db/migrations/000004_messages.up.sql
@@ -3,8 +3,10 @@ CREATE TABLE messages (
     room_id VARCHAR(36) NOT NULL,
     user_id VARCHAR(36) NULL,
     content TEXT NOT NULL,
+    reply_to_id VARCHAR(36) NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (room_id) REFERENCES rooms (id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL,
+    FOREIGN KEY (reply_to_id) REFERENCES messages (id) ON DELETE SET NULL
 );
