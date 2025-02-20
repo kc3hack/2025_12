f0246d427296501817eeb32fee15b3715ba2419e
saku39 <daisaku15@icloud.com>
2025-02-20 15:22:35 +0900

    feat: 検索バーと並び替えボタンの表示

--- web/src/app/home/page.tsx
+++ web/src/app/home/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import { Chat } from "@/features/routes/home/groups";
 import { Friends } from "@/features/routes/home/header";
+import {  Searchinput } from "@/features/routes/home/search";
+
 const home = () => {
   return (
     <div>
       <Friends />
+      <Searchinput/>
       <Chat />
     </div>
   );
