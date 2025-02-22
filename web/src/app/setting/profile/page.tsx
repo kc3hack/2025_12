b05c758be3af37b79f82af51f7905923b2464f8f
MgmgCandy <hayato.yamaoka.0316@gmail.com>
2025-02-22 12:43:44 +0900

    少しだけ修正。

--- web/src/app/setting/profile/page.tsx
+++ web/src/app/setting/profile/page.tsx
@@ -2,44 +2,12 @@
 import style from "../../../features/routes/settings-scss/page.module.scss";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
 
 const Profile = () => {
   return (
-    <>
-      <div className={style.profile_container}>
-        <div className={style.profile}>
-          <p className={style.my_profile}>My Profile</p>
-          <Avatar className={style.image}>
-      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"  />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
-        </div>
-        <div className={style.namebox_container}>
-          <p>お名前</p>
-          <input className={style.namebox} />
-          <div className={style.intro_container}>
-            <p>自己紹介</p>
-            <input className={style.intro} />
-          </div>
-        </div>
-      </div>
-    </>
+    <div>
+      <p>あいうえお</p>
+    </div>
   );
 };
 
