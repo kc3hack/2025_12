4c4373e57106ad1dd3f295583fc44858be8ac410
MgmgCandy <hayato.yamaoka.0316@gmail.com>
2025-02-22 12:39:51 +0900

    shadcnに置き換えた。

--- web/src/app/setting/profile/page.tsx
+++ web/src/app/setting/profile/page.tsx
@@ -1,14 +1,34 @@
 "use client";
 import style from "../../../features/routes/settings-scss/page.module.scss";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 
 const Profile = () => {
   return (
     <>
       <div className={style.profile_container}>
         <div className={style.profile}>
-          <p>My Profile</p>
-          <Image src="/globe.svg" alt="pp" width={100} height={100} className={style.image} />
+          <p className={style.my_profile}>My Profile</p>
+          <Avatar className={style.image}>
+      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn"  />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
         </div>
         <div className={style.namebox_container}>
           <p>お名前</p>
