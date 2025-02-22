4bdd54cf63e74da2ae43bddcb2615df4f55fc7b2
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-02-22 21:33:36 +0900

    chore: rootにアクセスしたら/home二リダイレクト

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,30 +1,7 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, useAuth, UserButton } from "@clerk/nextjs";
-import { apiClient } from "@/lib/apiClient";
+import { redirect } from "next/navigation";
 
 const Home = () => {
-  const { getToken } = useAuth();
-
-  return (
-    <div>
-      <Button>Push me</Button>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <Button
-        onClick={async () => {
-          await apiClient.get_user_me({ headers: { Authorization: `Bearer ${await getToken()}` } });
-        }}
-      >
-        Push me
-      </Button>
-    </div>
-  );
+  redirect("/home");
 };
 
 export default Home;
