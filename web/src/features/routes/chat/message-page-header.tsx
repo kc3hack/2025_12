e477b96504947d3b6b1060f992da404170adec84
mutyuki <belandinging01@gmail.com>
2025-02-23 19:20:21 +0900

    feat: メッセージしたよせ

--- web/src/features/routes/chat/message-page-header.tsx
+++ web/src/features/routes/chat/message-page-header.tsx
@@ -4,22 +4,9 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export const MessagePageHeader = () => {
   return (
-    // TODO: ロゴは変えといて
     <div className={style.header_inner}>
       <div className={style.logo}>
-        <img src="https://see-ss.com/img/logo@2x.png" alt="ロゴ" />
-      </div>
-      <div>
-        <SignedIn>
-          <div className={style.header_login}>
-            <UserButton />
-          </div>
-        </SignedIn>
-        <SignedOut>
-          <Button className={style.header_login}>
-            <p>ログイン</p>
-          </Button>
-        </SignedOut>
+        <p>asdfa</p>
       </div>
     </div>
   );
